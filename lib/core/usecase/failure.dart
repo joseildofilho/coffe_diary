130aa624e5e378eb54f90e562a8c6eb78ad3df18
joseildo filho <filhojoseildo@gmail.com>
2022-02-13 22:41:58 -0300

    feat: adds notificationbundle to handle multiple notifications

--- lib/core/usecase/failure.dart
+++ lib/core/usecase/failure.dart
@@ -1,1 +1,13 @@
 abstract class Failure {}
+
+class NotificationBundle extends Failure {
+    Iterable<Notification> notifications;
+
+    NotificationBundle(this.notifications);
+}
+
+class Notification {
+    final String message;
+
+    Notification({required this.message});
+}
