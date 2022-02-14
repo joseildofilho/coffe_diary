29c001b006c28bc11444d3917111c833bdda9158
joseildo filho <filhojoseildo@gmail.com>
2022-02-13 22:59:24 -0300

    improv: improves create_brew_register to notify some invalid parameters

--- lib/domain/usecases/create_brew_register.dart
+++ lib/domain/usecases/create_brew_register.dart
@@ -1,14 +1,54 @@
 import 'package:coffe_diary/core/usecase/failure.dart';
 import 'package:coffe_diary/core/usecase/success.dart';
 import 'package:coffe_diary/core/usecase/usecases.dart';
-import 'package:fpdart/src/task_either.dart';
+import 'package:equatable/equatable.dart';
+import 'package:fpdart/fpdart.dart';
 
 class CreateBrewRegister extends UseCase<CreateBrewRegisterParams, Success> {
   @override
   TaskEither<Failure, Success> call(CreateBrewRegisterParams param) {
-    // TODO: implement call
-    throw UnimplementedError();
+    return _validateInput(param).toTask();
+  }
+
+  IOEither<NotificationBundle, Success> _validateInput(
+      CreateBrewRegisterParams param) {
+    final notifications = <Notification>[];
+
+    if (param.coffeeName.isEmpty) {
+      notifications.add(const InvalidCoffeeName());
+    }
+
+    if (param.coffeeQuantity <= 0.0) {
+      notifications.add(const InvalidCoffeQuantity());
+    }
+
+    if (notifications.isEmpty) {
+      return IOEither.of(success);
+    }
+    return IOEither.left(NotificationBundle(notifications));
   }
 }
 
-class CreateBrewRegisterParams extends Failure {}
+class CreateBrewRegisterParams extends Equatable {
+  final String coffeeName, ratio;
+  final double coffeeQuantity;
+  final Duration bloomTime;
+
+  const CreateBrewRegisterParams({
+    required this.coffeeName,
+    required this.coffeeQuantity,
+    required this.ratio,
+    required this.bloomTime,
+  });
+
+  @override
+  List<Object?> get props => [coffeeName, coffeeQuantity, ratio, bloomTime];
+}
+
+class InvalidCoffeeName extends Notification {
+  const InvalidCoffeeName() : super(message: 'InvalidCoffeeName');
+}
+
+class InvalidCoffeQuantity extends Notification {
+  const InvalidCoffeQuantity() : super(message: 'InvalidCoffeQuantity');
+}
