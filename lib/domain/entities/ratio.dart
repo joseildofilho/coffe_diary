b6957cc924e22b346b9542259d834f5d299280a5
joseildo filho <filhojoseildo@gmail.com>
2022-02-25 23:29:11 -0300

    improv: adds validation fro ratio

--- lib/domain/entities/ratio.dart
+++ lib/domain/entities/ratio.dart
@@ -1,11 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../core/usecase/failure.dart';
 
 class Ratio extends Equatable {
   final String _ratio;
-  const Ratio({required String ratio}) : _ratio = ratio;
+  const Ratio._internal(this._ratio);
 
   String get ratio => _ratio;
+
+  static IOEither<Failure, Ratio> build({required String ratio}) {
+    if (ratio.isEmpty) IOEither.left(RatioCannotBeEmpty());
+    return IOEither.of(Ratio._internal(ratio));
+  }
 
   @override
   List<Object?> get props => [_ratio];
 }
+
+class RatioCannotBeEmpty extends Failure {}
